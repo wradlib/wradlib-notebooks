0f46c88c8456c81d4b467e1739fbca2d67d9fbd2
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2018-02-27 19:05:26 +0100

    MNT: add LICENSE.TXT, README.md, change copyright year

---
+++ README.md
@@ -0,0 +1,5 @@
+# README #
+
+**wradlib-notebooks**
+
+This repository consists of jupyter notebooks to give examples of wradlib usage and help users via tutorials.
--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-# Copyright (c) 2016, wradlib developers.
+# Copyright (c) 2018, wradlib developers.
 # Distributed under the MIT License. See LICENSE.txt for more info.
