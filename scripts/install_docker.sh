644daf2c5e1ac71323a236b2eaf01eb4a167ecea
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2018-10-19 13:21:12 +0200

    MIN: remove wradlib version test

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -28,5 +28,5 @@ python -c "import matplotlib; print('MATPLOTLIB:', matplotlib.__version__)"
 python -c "import netCDF4; print('NETCDF4:', netCDF4.__version__, netCDF4.getlibversion(), netCDF4.__hdf5libversion__)"
 python -c "import h5py; print('H5PY:', h5py.__version__, h5py.version.hdf5_version)"
 python -c "import xmltodict; print('XMLTODICT:', xmltodict.__version__)"
-python -c "import wradlib; print('WRADLIB:', wradlib.__version__, wradlib.__git_revision__)"
+# python -c "import wradlib; print('WRADLIB:', wradlib.__version__, wradlib.__git_revision__)"
 EOF
