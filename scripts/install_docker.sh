7dcdfb74e09e4d493f602927c23996a20620662c
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2019-08-02 12:12:39 +0200

    FIX: fix update xarray

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -11,7 +11,7 @@ conda config --set show_channel_urls True
 conda config --add channels conda-forge
 
 source activate wradlib
-conda update xarray
+conda update --yes xarray
 
 cd /home/build/wradlib
 python -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
