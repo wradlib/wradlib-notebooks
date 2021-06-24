a44f5c1f8e53504898f00daec925d9758ee5778b
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2021-06-24 07:43:02 +0200

    MNT: rename master -> main

--- binder/wradlib_data.sh
+++ binder/wradlib_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd ~
-wget https://github.com/wradlib/wradlib-data/archive/master.zip
-unzip master.zip
+wget https://github.com/wradlib/wradlib-data/archive/main.zip
+unzip main.zip
